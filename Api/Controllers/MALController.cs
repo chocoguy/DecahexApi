d712629fafc827d837f3c7ccaf38db657dadec6b
Edgar Zarco <chocoguy@mailfence.com>
2023-12-13 22:56:39 -0800

    Some progress

--- Api/Controllers/MALController.cs
+++ Api/Controllers/MALController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Api.Models;
+using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
@@ -6,6 +8,25 @@ namespace Api.Controllers
     [Route("[controller]")]
     public class MALController : ControllerBase
     {
+        private readonly ILogger<MALController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _configuration;
+
+        public MALController(ILogger<MALController> logger, IHttpClientFactory clientFactory, IConfiguration configuration)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+        }
+
+        [HttpGet("Anime/{malId}")]
+        public async Task<AnimeRecord> GetAnimeInfo(int malId)
+        {
+            AnimeRecord currAnimeRecord = new AnimeRecord();
+
+
+            return currAnimeRecord;
+        }
 
     }
 }
---
+++ Api/Models/MALRecord.cs
@@ -0,0 +1,64 @@
+﻿namespace Api.Models
+{
+    public class Broadcast
+    {
+        public string day_of_the_week { get; set; }
+        public string start_time { get; set; }
+    }
+
+    public class MainPicture
+    {
+        public string medium { get; set; }
+        public string large { get; set; }
+    }
+    public class MALRecord
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public MainPicture? main_picture { get; set; }
+        public int num_episodes { get; set; }
+        public string? source { get; set; }
+        public string media_type { get; set; }
+        public StartSeason? start_season { get; set; }
+        public Broadcast? broadcast { get; set; }
+        public string? start_date { get; set; }
+        public string? end_date { get; set; }
+        public string? synopsis { get; set; }
+        //nullable
+        public int rank { get; set; }
+        public string status { get; set; }
+        public List<Studio> studios { get; set; }
+        //nullable
+        public double mean { get; set; }
+        public Statistics statistics { get; set; }
+    }
+
+    public class StartSeason
+    {
+        public int year { get; set; }
+        public string season { get; set; }
+    }
+
+    public class Statistics
+    {
+        public Status status { get; set; }
+        public int num_list_users { get; set; }
+    }
+
+    public class Status
+    {
+        public string watching { get; set; }
+        public string completed { get; set; }
+        public string on_hold { get; set; }
+        //nullable
+        public string dropped { get; set; }
+        public string plan_to_watch { get; set; }
+    }
+
+    public class Studio
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+}
